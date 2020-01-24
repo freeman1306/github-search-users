859b6fb4cd314e6e64d31c0435f0b44deeee762d
freeman1306 <zavalkin06@gmail.com>
2020-01-24 21:10:07 +0200

    dividing on modules

--- main.js
+++ main.js
@@ -1,70 +1,9 @@
-class View {
-    constructor(){
-        this.app = document.getElementById('app')
-        
-        this.title = this.createElement('h1', 'title')
-        this.title.textContent = 'Github Search Users'
-
-        this.searchLine = this.createElement('div', 'search-line')
-        this.searchInput = this.createElement('input', 'search-input')
-        this.searchCounter = this.createElement('span', 'counter')
-        this.searchLine.append(this.searchInput)
-        this.searchLine.append(this.searchCounter)
+import {LOG} from './module/log.js'
+import {API} from './module/api.js'
+import {VIEW} from './module/view.js'
+import { Search } from './module/search.js'
 
 
-        this.usersWrapper = this.createElement('div', 'users-wrapper')
-        this.usersList = this.createElement('ul', 'users')
-        this.usersWrapper.append(this.usersList)
+const api = new API();
 
-
-        this.main = this.createElement('div', 'main')
-        this.main.append(this.usersWrapper)
-
-
-        this.app.append(this.title)
-        this.app.append(this.searchLine)
-        this.app.append(this.main)
-       
-
-
-      
-    }
-
-    createElement(elementTag, elementClass) {
-        const element = document.createElement(elementTag)
-
-        if (elementClass) {
-            element.classList.add(elementClass)
-        }
-        return element
-    }
-}
-
-class Search{
-    constructor(view) {
-        this.view = view
-        
-        this.view.searchInput.addEventListener('keyup', this.searchUsers.bind(this))
-   }
-    
-    async searchUsers() {
-        return await fetch(`https://api.github.com/search/users?q=${this.view.searchInput.value}`).then((res) => {
-            if (res.ok) {
-                res.json().then(
-                    res => {
-                        console.log(res);
-                        
-                    }
-                )
-            } else {
-                
-            }
-        })
-    }
-
-  
-
-
-}
-
-new Search(new View())
+new Search(new LOG(), api, new VIEW(api));
